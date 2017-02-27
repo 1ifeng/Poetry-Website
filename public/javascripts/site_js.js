ff6be86e27fc268aa2e74f365a04a410a0bd5912
lifeng1893 <lifeng.isme@foxmail.com>
2017-02-27 18:28:56 +0800

    增加上传头像 图片预览功能

--- public/javascripts/site_js.js
+++ public/javascripts/site_js.js
@@ -2,7 +2,7 @@ $(function() {
   $('.avatar').click(function() {
     $(this).toggleClass('dropdown');
   });
-
+  // 点赞功能 ajax提交
   $('.stat-appreciations').on('click', function() {
     var poetryId = $(this).parents('.poetry-item').attr('data-poetryid');
     var likesNum = Number($(this).text());
@@ -34,3 +34,18 @@ $(function() {
   }
 });
 
+// 头像预览 html5 FileReader()
+function showImage() {
+  var img = document.getElementById('upload-avatar').files[0];
+  var imgParent = img.parentNode;
+  var show = document.getElementById('show-avatar');
+  if (!/image\/\w+/.test(img.type)) {
+    alert('仅支持 jpg、png 格式大小 5M 以内图片。');
+    return false;
+  }
+  var reader = new FileReader();
+  reader.readAsDataURL(img);
+  reader.onload = function(e) {
+    show.style.backgroundImage = 'url(' + e.target.result + ')';
+  };
+}
